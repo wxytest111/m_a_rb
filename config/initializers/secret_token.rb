d6ea79f42804e09a8586b77d372ba0639112dd3c
Xinyu <30540047@qq.com>
2015-02-12 22:36:23 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CiDemo::Application.config.secret_key_base = 'a06d27d8ef15da828baf0414af27aaf4c7d828b56b948a5cc9901e6e11f4e01ee8aa16138f2272a1f04b96b70f30777a57ca6f9fb5217384dbb152e1c0057b19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CiDemo::Application.config.session_store :cookie_store, key: '_ci-demo_session'
