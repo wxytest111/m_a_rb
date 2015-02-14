2af3dd0029f4bd38f81fd747b7987f56544a0a11
Xinyu <30540047@qq.com>
2015-02-14 17:48:43 +0530

    change order of deploy command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,16 +18,16 @@ fi;
 scp ci-demo.zip ${USER}@${HOST}:/tmp
 
 ssh ${USER}@${HOST} /bin/bash << EOF
-cp scripts/unicorn /etc/init.d/
-chmod 777 /etc/init.d/unicorn
-cp sciprts/ci-demo.conf /etc/nginx/conf.d/
-/etc/init.d/nginx restart
-ps -ef | grep unicorn | awk '{print $2}'|xargs kill -9
 rm -fr /opt/works/ci-demo
 mkdir -p /opt/works/ci-demo
 mv /tmp/ci-demo.zip /opt/works/ci-demo
 cd /opt/works/ci-demo
 unzip ci-demo.zip
+cp scripts/unicorn /etc/init.d/
+chmod 777 /etc/init.d/unicorn
+cp sciprts/ci-demo.conf /etc/nginx/conf.d/
+/etc/init.d/nginx restart
+ps -ef | grep unicorn | awk '{print $2}'|xargs kill -9
 bundle
 bundle exec rake db:migrate
 bundle exec rake assets:precompile
