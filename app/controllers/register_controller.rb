751e0a411e1b7ca42ac27aa27db909a329cdb64a
xinyu <30540047@qq.com>
2015-06-15 20:35:31 +0800

    not use check_code for now

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -26,51 +26,51 @@ class RegisterController < ApplicationController
     password = params[:password]
     check_codes= CheckCode.find_by_mobile(mobile)
     if check_codes
-      if (Time.now - check_codes.updated_at)>30*60
-        @result = 0
-        @error_code = 2
-        @error_msg = '验证码失效，请重新获取.'
-      elsif check_codes.code!=check_code
-        @result = 0
-        @error_code = 2
-        @error_msg = '验证码有误，请重新输入.'
-      else
-        if user_type == 1
-          customer = Customer.find_by_mobile(mobile)
-          if customer
-            @result = 0
-            @error_code = 3
-            @error_msg = '该手机已经被注册，请您直接登录.'
-          else
-            customer = Customer.new(mobile:mobile, password:password, nick_name:mobile)
-            token = Token.new(token:Digest::SHA1.hexdigest(Time.now.to_s))
-            customer.token = token
-            customer.save!
-            @result = 1
-            @token = token.token
-            @data = {user_type:user_type,customer_id:customer.id,mobile:mobile,nick_name:customer.nick_name,address:customer.address,status:0}
-          end
-        elsif user_type == 2
-          worker = Worker.find_by_mobile(mobile)
-          if worker
-            @result = 0
-            @error_code = 3
-            @error_msg = '该手机已经被注册，请您直接登录.'
-          else
-            worker = Worker.new(mobile:mobile, password:password)
-            token = Token.new(token:Digest::SHA1.hexdigest(Time.now.to_s))
-            worker.token = token
-            worker.save!
-            @result = 1
-            @token = token.token
-            @data = {user_type:user_type,worker_id:worker.id,mobile:mobile,name:worker.name,address:worker.address,status:0}
-          end
-        else
+      # if (Time.now - check_codes.updated_at)>30*60
+      #   @result = 0
+      #   @error_code = 2
+      #   @error_msg = '验证码失效，请重新获取.'
+      # elsif check_codes.code!=check_code
+      #   @result = 0
+      #   @error_code = 2
+      #   @error_msg = '验证码有误，请重新输入.'
+      # else
+      if user_type == 1
+        customer = Customer.find_by_mobile(mobile)
+        if customer
           @result = 0
-          @error_code = 10002
-          @error_msg = '用户类型错误.'
+          @error_code = 3
+          @error_msg = '该手机已经被注册，请您直接登录.'
+        else
+          customer = Customer.new(mobile:mobile, password:password, nick_name:mobile)
+          token = Token.new(token:Digest::SHA1.hexdigest(Time.now.to_s))
+          customer.token = token
+          customer.save!
+          @result = 1
+          @token = token.token
+          @data = {user_type:user_type,customer_id:customer.id,mobile:mobile,nick_name:customer.nick_name,address:customer.address,status:0}
         end
+      elsif user_type == 2
+        worker = Worker.find_by_mobile(mobile)
+        if worker
+          @result = 0
+          @error_code = 3
+          @error_msg = '该手机已经被注册，请您直接登录.'
+        else
+          worker = Worker.new(mobile:mobile, password:password)
+          token = Token.new(token:Digest::SHA1.hexdigest(Time.now.to_s))
+          worker.token = token
+          worker.save!
+          @result = 1
+          @token = token.token
+          @data = {user_type:user_type,worker_id:worker.id,mobile:mobile,name:worker.name,address:worker.address,status:0}
+        end
+      else
+        @result = 0
+        @error_code = 10002
+        @error_msg = '用户类型错误.'
       end
+      # end
     end
   end
 
