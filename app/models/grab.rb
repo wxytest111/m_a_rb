e0271ddb35227bd730a93a4acf4ac3c144774ac2
xinyu <30540047@qq.com>
2015-06-18 22:28:52 +0800

    s

--- app/models/grab.rb
+++ app/models/grab.rb
@@ -1,5 +1,5 @@
 class Grab < ActiveRecord::Base
-  enum status: [:start, :waiting, :ready, :begin, :finished, :cancel, :abort, :other_worker, :time_out]
+  enum status: [:start, :waiting, :ready, :begin, :finished, :cancel, :other_worker, :time_out]
 
   belongs_to :worker
   belongs_to :appointment
