f0b7290d95894732ea96d6218a457a60ed498b74
xinyu <30540047@qq.com>
2015-06-25 23:37:08 +0800

    s

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ActiveRecord::Base
+  enum status: [:start, :choose, :unpaid, :time_out, :paid, :leaving, :service, :finish]
+
   belongs_to :street
   belongs_to :customer
   belongs_to :worker
